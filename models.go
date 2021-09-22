c796e1f71a4c2a4e9951fb2bdb0424bd403849e8
tod91 <todorivanov066@gmail.com>
2021-09-22 11:50:16 +0300

    Cleaned up linter warnings

--- models.go
+++ models.go
@@ -1,22 +1,37 @@
 package main
 
+// EvaluateReq ...
+//
+// used to store the request body when Evaluate is called
 type EvaluateReq struct {
 	Expression string `json:"expression"`
 }
 
+// EvaluateResp ...
+//
+// used to store the response body when Evaluate is called
 type EvaluateResp struct {
 	Result string `json:"result"`
 }
 
+// ValidateReq ...
+//
+// used to store the request  body when Validate is called
 type ValidateReq struct {
 	Expression string `json:"expression"`
 }
 
+// ValidateResp ...
+//
+// used to store the response  body when Validate is called
 type ValidateResp struct {
 	Valid  bool   `json:"valid"`
 	Reason string `json:"reason"`
 }
 
+// ErrorsResp ...
+//
+// used to store the response  body when errors is called
 type ErrorsResp struct {
 	Expression string `json:"expression"`
 	Endpoint   string `json:"endpoint"`
